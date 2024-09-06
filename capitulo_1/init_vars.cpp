9b52c5c9273e0ace44631588d762e4e80c55e8d3
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-06 05:26:27 -0300

    init vars

---
+++ capitulo_1/init_vars.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int evento = 5;
+	char corrida = 'C';
+	float tempo = 27.25;
+	cout << "\nO tempo vitorioso na eliminatória "
+		<< corrida << "\nda competição " << evento 
+		<< " foi " << tempo << '.' << endl;
+	system("PAUSE");
+	return 0;
+}
