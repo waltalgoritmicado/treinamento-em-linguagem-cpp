16909aafe44a94ad14e955b4aacbde8b21bcb04b
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-06 05:23:50 -0300

    variáveis unsigned (sem sinal)

---
+++ unsig_vars.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned j = 4294967000UL;
+	
+	cout << "\nVariável unsigned = " << j;
+	int i = j;
+
+	cout << "\nVariável int = " << i << endl;
+
+	system("PAUSE");
+	return 0;
+}
