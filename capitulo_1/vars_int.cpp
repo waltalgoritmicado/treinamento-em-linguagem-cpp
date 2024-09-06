e0475830e3c793052be5679b5ed22c8a475b9f07
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-06 05:00:35 -0300

    vars_int

---
+++ capitulo_1/vars_int.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int num1;
+	num1 = 44;
+
+	int num2;
+	num2 = num1 + 20;
+
+	cout << "\no primeiro número é " << num1;
+	cout << "\no segundo número é \" num1 + 20 \" = " << num2 << endl;
+
+	system("PAUSE");
+	return 0;
+}
