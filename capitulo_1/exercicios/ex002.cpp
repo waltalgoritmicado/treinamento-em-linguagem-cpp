b6f0aee0074f3faa26e0a93ceb225565cfd75a8b
Walt Algoritmicado <waltalgoritmicado@gmail.com>
2024-09-06 14:24:00 -0300

    ex002

---
+++ capitulo_1/exercicios/ex002.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {	
+	cout << "\n\tBom dia! Shirley.";
+	cout << "Você já tomou café? \n";
+	cout << "\n\nA solucão não existe\nNão insista.";
+	cout << "Duas\tlinhas\tde\tsáida\nou\tuma";
+	cout << "um"<<"\n"<<"dois"<<"\n"<<"três\n";
+	return 0;
+}
